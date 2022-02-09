75344dd4c17d62ec6c0ce1ebd4ad5fad6aef4c8e
dhanushcrueiso <dhanushravula@gmail.com>
2022-02-09 12:45:51 +0530

    feb9

--- Codes/Longest_subarray_without_repeating.java
+++ Codes/Longest_subarray_without_repeating.java
@@ -21,7 +21,31 @@ public class Longest_subarray_without_repeating {
             }
           return max;  
         }
+
+
+
     
+
+
+
+public int findPairs(int[] nums, int k) {
+        
+    //hashmap is more power tool for the this question instead of hashset
+    //set the value,frequency pair in the hashmap
+    HashMap<Integer, Integer> map = new HashMap<>();
+    
+    for(int n: nums) {
+        map.put(n, map.getOrDefault(n, 0) + 1);
+    }
+    
+    int cnt = 0;
+    for(int n: map.keySet()) {
+        if(k > 0 && map.containsKey(n + k) || k == 0 && map.get(n) > 1) {
+            cnt++;
+        }
+    }
+    return cnt;
+}
 }
 
 
